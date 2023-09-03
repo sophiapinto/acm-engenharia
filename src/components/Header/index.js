1c188531111e20141a83195cb43c46348d8520c5
Sophia Pinto <sarahsophiapinto@hotmail.com>
2023-09-03 20:00:09 -0300

    Create Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,20 @@
+import logo from '../../assets/logo.png';
+import './header.css';
+
+function Header() {
+    
+  return (
+    <div className="header">
+     <img src={logo} className="logo-acm" alt="logo" />
+        
+        <div class="topnav">
+            <a href="#quem-somos">Quem somos</a>
+            <a href="#servicos">Serviços</a>
+            <a href="#portfolio">Portfólio</a>
+            <a href="#contato">Contato</a>
+        </div>
+    </div>
+  );
+}
+
+export default Header;
