3eddc1388f88edcba4f75569fbf39c823668d83c
Sophia Pinto <sarahsophiapinto@hotmail.com>
2023-09-04 16:19:15 -0300

    Ajustes CSS do Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -11,13 +11,13 @@ function Footer() {
           <span className='footer-text'>Copyright&copy;&nbsp;2023.<br/>Desenvolvido por Sophia Pinto.</span>
           
           <div class="footer-nav">
-              <a href="#quem-somos">
+              <a href="https://www.linkedin.com/company/acm-engenharia-civil/">
                 <img src={linkedin} className="logo" alt="logo-linkedin" />
               </a>
-              <a href="#quem-somos">
+              <a href="https://instagram.com/acm_engenhariacivil?igshid=MmU2YjMzNjRlOQ==">
                 <img src={instagram} className="logo" alt="logo-linkedin" />
               </a>
-              <a href="#quem-somos">
+              <a href="https://wa.me/9881279583">
                 <img src={whatsapp} className="logo" alt="logo-linkedin" />
               </a>
           </div>
